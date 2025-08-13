60e511c2563277637770316d0869ae505b7e2360
Bhavik <97172550+BhavikNaik@users.noreply.github.com>
2025-08-13 16:44:05 +0530

    minor 2

--- src/app-pushapp/views/admin/push-notification/previews/PopOverPreview.vue
+++ src/app-pushapp/views/admin/push-notification/previews/PopOverPreview.vue
@@ -130,7 +130,7 @@ onMounted(()=>{})
   transform: translate(-50%, -50%);
   width: 100%;
   /* max-width: 380px; */
-  /* aspect-ratio: 9 / 16; */
+  aspect-ratio: 9 / 16;
   background-color: rgb(255, 255, 255);
   border-radius: 20px;
   position: relative;
