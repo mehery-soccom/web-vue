02e129c269ca3f3dd1c3c64753c91e79bb5606fd
Bhavik <97172550+BhavikNaik@users.noreply.github.com>
2025-10-16 15:15:22 +0530

    notebook clear 3

--- src/app-notebook/navigation/vertical/admin.js
+++ src/app-notebook/navigation/vertical/admin.js
@@ -5,10 +5,10 @@ export default [
     children: [
       // { title: "view two", to: "views-two" },
       // { title: "view createfaqs", to: "views-createfaqs" },
-      { title: "Faqs", to: "views-faqsTable" },
+      { title: "Faqs", to: "views-faqstable" },
       // { title: "knowledge base", to: "views-knowledgebase" },
       // { title: "topic", to: "views-topic" },
-      { title: "Articles for rag" , to : "views-Articles"},
+      { title: "Articles for rag" , to : "views-articles"},
       // { title: "view faqs2" , to : "views-faqs2"}
     ],
   },
--- src/app-notebook/router/index.js
+++ src/app-notebook/router/index.js
@@ -1,8 +1,8 @@
 import { BootRouter } from "@/@common";
 import { CDN_CONTEXT } from "@/@common/constants";
 import { routes } from "vue-router/auto-routes";
-import Articles from "@/app-notebook/pages/views/Articles/index.vue";
-import FaqsTable from "@/app-notebook/pages/views/faqsTable/index.vue";
+import Articles from "@/app-notebook/pages/views/articles/index.vue";
+import FaqsTable from "@/app-notebook/pages/views/faqstable/index.vue";
 import Dashboard from "@/app-notebook/pages/dashboards/analytics.vue";
 import DefaultLayout from "@/app-notebook/layouts/default.vue";
 
@@ -34,12 +34,12 @@ if (!routes || routes?.length < 3) {
       ],
     },
     {
-      path: "/views/Articles",
+      path: "/views/articles",
       component: DefaultLayout,
       children: [
         {
           path: "",
-          name: "views-Articles",
+          name: "views-articles",
           component: Articles,
           props: true,
           meta: { layout: "default" },
@@ -47,12 +47,12 @@ if (!routes || routes?.length < 3) {
       ],
     },
     {
-      path: "/views/faqsTable", 
+      path: "/views/faqstable", 
       component: DefaultLayout,
       children: [
         {
           path: "",
-          name: "views-faqsTable", 
+          name: "views-faqstable", 
           component: FaqsTable,
           props: true,
           meta: { layout: "default" },
