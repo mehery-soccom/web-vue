f5fdd4826d26d8eaffd42a7f94af3652f05afcd6
Vivek Pandit <vinodpandit210@gmail.com>
2026-01-02 18:52:00 +0530

    a landing page added for call page can access at /call

--- src/app-phone/router/index.js
+++ src/app-phone/router/index.js
@@ -4,6 +4,7 @@ import { routes } from "vue-router/auto-routes";
 import Call from "@/app-phone/pages/call/[id].vue";
 import Dialer from "@/app-phone/pages/dialer/index.vue";
 import BlankLayout from "@/app-phone/layouts/blank.vue";
+import Index from "../pages/call/index.vue";
 
 console.log("phone routes", routes)
 export default BootRouter.route({
@@ -28,6 +29,11 @@ export default BootRouter.route({
           meta: { layout: "blank" },
         },
       ],
+    },
+    {
+      path:"/call",
+      component: Index,
+      name: "call"
     },
     {
       path: "/call/:id",
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import "./preloader";
-
+if (window.CONST) window.CONST.APP = "phone";
 import { BootLoader } from "@/@common";
 import appConfig from "@/app.config.js";
 
