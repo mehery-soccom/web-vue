ae6612fb68c5fd9725994d6736bc1ff92314bead
Bhavik <97172550+BhavikNaik@users.noreply.github.com>
2026-03-28 18:58:17 +0530

    filters support for date, stamp instead of dateLocal and stampUTC

--- src/app-pushapp/views/admin/app-engagements/FilterViewer.vue
+++ src/app-pushapp/views/admin/app-engagements/FilterViewer.vue
@@ -63,11 +63,11 @@ const indent = computed(() => ({
           <template v-else>
             <strong>{{ formatFieldName(node.field) }}</strong>
             {{ formatFieldName(node.operator) }}
-            <strong v-if="node.operator === 'BETWEEN' && node.value?.length === 2 && node.value[0]?.dateLocal">
-              {{ node.value[0].dateLocal }} and {{ node.value[1].dateLocal }}
+            <strong v-if="node.operator === 'BETWEEN' && node.value?.length === 2 && (node.value[0]?.dateLocal || node.value[0]?.date)">
+              {{ node.value[0].dateLocal || node.value[0].date }} and {{ node.value[1].dateLocal || node.value[1].date }}
             </strong>
-            <strong v-else-if="node.value?.[0]?.dateLocal">
-              {{ node.value[0].dateLocal }}
+            <strong v-else-if="node.value?.[0]?.dateLocal || node.value?.[0]?.date">
+              {{ node.value[0].dateLocal || node.value[0].date }}
             </strong>
             <strong v-else-if="Array.isArray(node.value)">
               {{ node.value.join(", ") }}
